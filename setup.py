0dc2cc85de295e24728505f1a568bdeb6ae3f723
misulik112 <ursu.alin89@gmail.com>
2024-03-19 22:27:33 +0200

    Commenting the clone repo

--- setup.py
+++ setup.py
@@ -28,19 +28,19 @@ def install_packages():
         logging.error(f"Failed to install packages: {e}")
 
 
-def clone_git_repository(repo_url, destination):
-    """
-    Clone a Git repository.
-
-    Args:
-    - repo_url (str): The URL of the Git repository to clone.
-    - destination (str): The destination directory where the repository will be cloned.
-    """
-    try:
-        subprocess.run(["git", "clone", repo_url, destination], check=True)
-        logging.info(f"Cloned repository from {repo_url} to {destination}")
-    except subprocess.CalledProcessError as e:
-        logging.error(f"Failed to clone repository: {e}")
+# def clone_git_repository(repo_url, destination):
+#     """
+#     Clone a Git repository.
+#
+#     Args:
+#     - repo_url (str): The URL of the Git repository to clone.
+#     - destination (str): The destination directory where the repository will be cloned.
+#     """
+#     try:
+#         subprocess.run(["git", "clone", repo_url, destination], check=True)
+#         logging.info(f"Cloned repository from {repo_url} to {destination}")
+#     except subprocess.CalledProcessError as e:
+#         logging.error(f"Failed to clone repository: {e}")
 
 
 def adding_configs():
@@ -68,9 +68,9 @@ def adding_configs():
             os.symlink(source_path, destination_path)
             logging.debug(f"Linked {source_path} to {destination_path}")
 
-        # Clone the tmux plugin manager repository
-        clone_git_repository(
-            "https://github.com/tmux-plugins/tpm", "~/.tmux/plugins/tpm"
+        # # Clone the tmux plugin manager repository
+        # clone_git_repository(
+        #     "https://github.com/tmux-plugins/tpm", "~/.tmux/plugins/tpm"
         )
         logging.info("Configurations added successfully.")
     except Exception as e:
